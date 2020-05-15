c4d4e936a26cc73a5de0c982f304645e5baff7f5
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-15 18:21:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+  return (x + y)
+}
+
+function subtract(x, y) {
+  return (y - x)
+}
+
+function multiply(x, y) {
+  return (x * y)
+}
+
+function divide(x, y) {
+  return (x / y)
+}
